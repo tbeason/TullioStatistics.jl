0e1c8b3bd1fb1c3451c5d20905b01440364497c3
Tyler Beason <tbeas12@gmail.com>
2021-11-16 10:43:16 -0500

    more work

--- src/TullioStatistics.jl
+++ src/TullioStatistics.jl
@@ -11,18 +11,17 @@ using LoopVectorization
 ###### BASIC FUNCTIONS
 
 sum(A::AbstractVector{T}) where {T} = @tullio (+) R := A[i]
-sum(f::Function,A::AbstractVector{T}) where {T} = @tullio (+) R := f(A[i])
-sum(f::Function,A::AbstractVector{T},c) where {T} = @tullio (+) R := f(A[i]-c)
+sum(f::Function,A::AbstractVector{T},c=0.0) where {T} = @tullio (+) R := f(A[i]-c)
 
 
 prod(A::AbstractVector{T}) where {T} = @tullio (*) R := A[i]
-prod(f::Function,A::AbstractVector{T}) where {T} = @tullio (*) R := f(A[i])
+prod(f::Function,A::AbstractVector{T},c=0.0) where {T} = @tullio (*) R := f(A[i]-c)
 
 maximum(A::AbstractVector{T}) where {T} = @tullio (max) R := A[i]
-maximum(f::Function,A::AbstractVector{T}) where {T} = @tullio (max) R := f(A[i])
+maximum(f::Function,A::AbstractVector{T},c=0.0) where {T} = @tullio (max) R := f(A[i]-c)
 
 minimum(A::AbstractVector{T}) where {T} = @tullio (min) R := A[i]
-minimum(f::Function,A::AbstractVector{T}) where {T} = @tullio (min) R := f(A[i])
+minimum(f::Function,A::AbstractVector{T},c=0.0) where {T} = @tullio (min) R := f(A[i]-c)
 
 
 function sum(A::AbstractMatrix{T};dims=:) where {T}
@@ -32,11 +31,18 @@ function sum(A::AbstractMatrix{T};dims=:) where {T}
     dims > 2 && return A
 end
 
-function sum(f::Function,A::AbstractMatrix{T};dims=:) where {T}
-    (isequal(dims,:) || isequal(dims,(1,2))) && (return @tullio (+) R := f(A[i,j]))
-    isequal(dims,1) && (return @tullio (+) R[1,j] := f(A[i,j]))
-    isequal(dims,2) && (return @tullio (+) R[i] := f(A[i,j]))
-    dims > 2 && return f.(A)
+function sum(f::Function,A::AbstractMatrix{T},c::Number=0.0;dims=:) where {T}
+    (isequal(dims,:) || isequal(dims,(1,2))) && (return @tullio (+) R := f(A[i,j]-c))
+    isequal(dims,1) && (return @tullio (+) R[1,j] := f(A[i,j]-c))
+    isequal(dims,2) && (return @tullio (+) R[i] := f(A[i,j]-c))
+    dims > 2 && return f.(A.-c)
+end
+function sum(f::Function,A::AbstractMatrix{T},c::AbstractVecOrMat{S};dims=1) where {T,S}
+    N,K = size(c)
+    @assert (isequal(dims,:) || isequal(dims,(1,2))) "`dims` "
+    isequal(dims,1) && (return @tullio (+) R[1,j] := f(A[i,j]-c[i]))
+    isequal(dims,2) && (return @tullio (+) R[i] := f(A[i,j]-c[j]))
+    dims > 2 && return f.(A.-c)
 end
 prod(A::AbstractMatrix{T}) where {T} = @tullio (*) R := A[i]
 maximum(A::AbstractMatrix{T}) where {T} = @tullio (max) R := A[i]
@@ -55,9 +61,9 @@ function mean(A::AbstractVector{T}) where {T}
     return sum(A)/N
 end
 
-function mean(f::Function,A::AbstractVector{T}) where {T}
+function mean(f::Function,A::AbstractVector{T},c=0.0) where {T}
     N = length(A)
-    return sum(f,A)/N
+    return sum(f,A,c)/N
 end
 
 
@@ -70,13 +76,13 @@ function mean(A::AbstractMatrix{T};dims=:) where {T}
     dims > 2 && return A
 end
 
-function mean(f::Function,A::AbstractMatrix{T};dims=:) where {T}
+function mean(f::Function,A::AbstractMatrix{T},c=0.0;dims=:) where {T}
     N,K = size(A)
     Y = N*K
-    isequal(dims,:) && return sum(f,A)/Y
-    isequal(dims,1) && return sum(f,A,dims=1)/N
-    isequal(dims,2) && return sum(f,A,dims=2)/K
-    dims > 2 && return f.(A)
+    isequal(dims,:) && return sum(f,A,c)/Y
+    isequal(dims,1) && return sum(f,A,c;dims=1)/N
+    isequal(dims,2) && return sum(f,A,c;dims=2)/K
+    dims > 2 && return f.(A.-c)
 end
 
 
@@ -85,15 +91,15 @@ end
 # VARM
 function varm(A::AbstractVector{T}, m; corrected::Bool=true) where {T}
     N = length(A)
-    top = sum(abs2,A,m)
-    res = corrected ? top/(N-1) : top/N
+    res = mean(abs2,A,m)
+    corrected && return res*N/(N-1)
     return res
 end
 
 function varm(f::Function,A::AbstractVector{T}, m; corrected::Bool=true) where {T}
     N = length(A)
-    res = sum(abs2 ∘ f,A)/N - m^2
-    res = corrected ? res*N/(N-1) : res
+    res = mean(abs2 ∘ f,A) - m^2
+    corrected && return res*N/(N-1)
     return res
 end
 
@@ -104,8 +110,10 @@ function varm(A::AbstractMatrix{T}, m; dims=:, corrected::Bool=true) where {T}
     if isequal(dims,:)
         @assert length(m) == 1 "Dimension mismatch between `m` and `dims`."
         return varm(vec(A),m; corrected=corrected)
-    elseif isequal(dims,1) 
-        return sum(A,dims=1)/N
+    elseif isequal(dims,1) & length(m)==1
+        top = sum(abs2,A,m; dims=1)
+        res = corrected ? top./(N-1) : top./N
+        return res
     elseif isequal(dims,2)
         return sum(A,dims=2)/K
     else
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,10 @@
 using TullioStatistics
 using Test
 using Statistics
+using StableRNGs
+
+rng = StableRNG(111)
+
+A = rand(rng,100);
+
+@test TullioStatistics.mean(A) ≈ mean(A)
